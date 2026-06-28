006dd52b677ef61d2f2ba4533d6bde4e9ecafe83
laugh0064 <guoj3475@gmail.com>
2026-06-28 19:16:00 +0800

    final

--- 上位机/waist_ui/ui/__init__.py
+++ 上位机/waist_ui/ui/__init__.py
@@ -1,12 +1,13 @@
 # coding: utf-8
 """
-UI模块
+UI 模块
 """
 
-from .main_window import MainWindow
-from .data_monitor import DataMonitorInterface
-from .rehab_training import PresetMotionInterface
-from .fun_game import FunGameInterface
-from .user_custom import UserCustomInterface
-
-__all__ = ['MainWindow', 'DataMonitorInterface', 'PresetMotionInterface', 'FunGameInterface', 'UserCustomInterface']
+__all__ = [
+    'MainWindow',
+    'DataMonitorInterface',
+    'PresetMotionInterface',
+    'FunGameInterface',
+    'UserCustomInterface',
+    'AiAnalysisInterface',
+]
